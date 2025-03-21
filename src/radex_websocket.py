4366c281f4454e26f00f249ad6fed94810672052
Tobbinloggin <53196506+Tobbinloggin@users.noreply.github.com>
2025-03-21 13:35:08 +0000

    Update radex_websocket.py

--- src/radex_websocket.py
+++ src/radex_websocket.py
@@ -20,7 +20,8 @@ async def broadcast_data(websocket, path):
         await asyncio.sleep(10)  # Delay before reading again
 
 async def main():
-    server = await websockets.serve(broadcast_data, "0.0.0.0", PORT)
+    server = websockets.serve(broadcast_data, "0.0.0.0", PORT)
+
     print(f"WebSocket server running on ws://0.0.0.0:{PORT}")
     await server.wait_closed()
 
